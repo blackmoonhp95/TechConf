922b1c0025ed87c08d4e0dfb6054c5f7ce5cca7c
Halley <blackmoon8668@gmail.com>
2022-04-29 02:54:26 +0700

    Fix bug

--- function/ServiceBusQueueTrigger/__init__.py
+++ function/ServiceBusQueueTrigger/__init__.py
@@ -1,53 +1,59 @@
 import logging
+from urllib import response
 import azure.functions as func
 import psycopg2
 import os
 from datetime import datetime
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from sendgrid.helpers.mail import Mail, Content
+import traceback
+
+
+api_key = os.getenv("SENDGRID_API_KEY")
+sg = SendGridAPIClient(api_key=api_key)
+from_email = "quangnmbk58@outlook.com.vn"
 
 
 def main(msg: func.ServiceBusMessage):
 
-    notification_id = int(msg.get_body().decode('utf-8'))
-    logging.info('Python ServiceBus queue trigger processed message: %s', notification_id)
-    
-    connect = psycopg2.connect(dbname="techconfdb",
-                               user="useracc@mypostserver",
-                               password="Maiyeuem@hp95",
-                               host="mypostserver.postgres.database.azure.com")
-
-    print("hello world!")
-
     try:
-        pass
+        notification_id = int(msg.get_body().decode('utf-8'))
+        logging.info('Python ServiceBus queue trigger processed message: %s', notification_id)
+        
+        connect = psycopg2.connect(dbname="techconfdb",
+                                user="useracc@mypostserver",
+                                password="Maiyeuem@hp95",
+                                host="mypostserver.postgres.database.azure.com")
+
+    
         with connect.cursor() as cur:
             cur.execute("SELECT message, subject FROM notification WHERE id = {}".format(notification_id))
-            notifications = cur.fetchall()
+            notification = cur.fetchone()
             cur.execute("SELECT first_name, email FROM attendee")
             attendees = cur.fetchall()
             
-            
-            logging.info(type(notifications))
-            logging.info(notifications)
+            logging.info(notification)
             logging.info(attendees)
         
-        for attendee in attendees:
-            pass
-        
-        with connect.cursor() as cur:
-            cur.execute("UPDATE notification SET status = 'Notified {} attendees' WHERE id = {}".format(len(attendees)
-                                                                                                        , notification_id))
-        # TODO: Get notification message and subject from database using the notification_id
+            for attendee in attendees:
+                to_email = attendee[1]
+                subject = notification[1]
+                content = Content("text/plain", notification[0])
+                mail = Mail(from_email, to_email, subject, content)
+                response = sg.send(mail)
 
-        # TODO: Get attendees email and name
-
-        # TODO: Loop through each attendee and send an email with a personalized subject
-
-        # TODO: Update the notification table by setting the completed date and updating the status with the total number of attendees notified
+            cur.execute("UPDATE notification \
+                        SET \
+                            status = 'Notified {} attendees', \
+                            completed_date =  NOW() \
+                        WHERE id = {}".format(len(attendees), 
+                                            notification_id))
 
     except Exception as error:
-        logging.error(error)
+        logging.error(traceback.format_exc())
+        return func.HttpResponse(traceback.format_exc())
     finally:
-        # TODO: Close connection
-        pass
+        connect.commit()
+        connect.close()
+    
+    
